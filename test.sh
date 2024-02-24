5f987d63016e4762dbb5e1b33f581350a3669b40
Ramesh <rameshb.dba86@gmail.com>
2024-02-24 07:53:13 +0530

    updated test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-echo "Hello Ramesh, I am from GitHub"
-hostname
-=======
 echo "Hello Ramesh, I am from GitHub"
 hostname
 echo "hello Prasanna"
 echo "Hello Divisha"
->>>>>>> cfa18f42edf23f9923ece67227d47e810a1aacdb
